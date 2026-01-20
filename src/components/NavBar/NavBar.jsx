23a01e3bd80b7d636722f38f778b31d5c897a3eb
Shaibee19 <shaipattzgray@gmail.com>
2026-01-19 20:34:31 -0500

    finished navbar, working on hero banner

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,10 @@
-import React from "react"
-import './NavBar.css'
-import logo from '../../assets/logo.png'
-import search_icon from '../../assets/search_icon.svg'
-import bell_icon from '../../assets/bell_icon.svg'
-import profile_img from '../../assets/profile_img.png'
-import caret_icon from '../../assets/caret_icon.svg'
+import React from "react";
+import "./NavBar.css";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search_icon.svg";
+import bell_icon from "../../assets/bell_icon.svg";
+import profile_img from "../../assets/profile_img.png";
+import caret_icon from "../../assets/caret_icon.svg";
 
 const name = (props) => {
   return (
@@ -24,15 +24,16 @@ const name = (props) => {
         <img src={search_icon} alt="" className="icons" />
         <p>Children</p>
         <img src={bell_icon} alt="" className="icons" />
-        <div className="navbar__profile"></div>
-        <img src={profile_img} alt="" className="profile" />
-        <img src={caret_icon} alt="" />
-        <div className="dropdown">
-          <p>Sign Out of Netflix</p>
+        <div className="navbar__profile">
+          <img src={profile_img} alt="" className="profile" />
+          <img src={caret_icon} alt="" />
+          <div className="dropdown">
+            <p>Sign Out of Netflix</p>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default name;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,32 @@
-import React from "react"
-import './Home.css'
-import NavBar from '../../components/NavBar/NavBar'
+import React from "react";
+import "./Home.css";
+import NavBar from "../../components/NavBar/NavBar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
 
 const name = (props) => {
   return (
     <div className="Home">
-      <NavBar/>
+      <NavBar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner__img" />
+        <div className="hero__caption">
+          <img src={hero_title} alt="" className="caption__img" />
+          <p>
+            Discovering his ties to a secret ancient order, a young man living
+            in modern Istanbul embarks on a quest to save the city from an
+            immortal enemy.
+          </p>
+          <div className="hero__btns">
+            <button><img src={play_icon} alt="" className="btn"/>Play</button>
+            <button><img src={info_icon} alt="" className="btn dark__btn"/>More Info</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default name;
